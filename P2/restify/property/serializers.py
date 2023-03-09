20d1edf92b2f2843119af12b16edefc8ce7cb4a6
Vincent Zhu <vincenthero16@gmail.com>
2023-03-09 00:33:55 -0500

    Property create,update,delete almost done 12:29 AM Vincent

--- P2/restify/property/serializers.py
+++ P2/restify/property/serializers.py
@@ -4,14 +4,14 @@ from .models import Property
 class PropertySerializer(ModelSerializer):
     class Meta:
         model = Property
-        fields = ['name', 'description', 'owner', 'location', 'available_dates', 'guest_capacity', 'amenities', 'rating', 'price']
+        fields = ['property_name', 'description', 'owner', 'location', 'available_dates', 'guest_capacity', 'amenities', 'rating', 'price']
 
     def create(self, validated_data):
         # print(self.context['request'].property)
         return Property.objects.create(validated_data)
     
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
+        instance.property_name = validated_data.get('property_name', instance.property_name)
         instance.description = validated_data.get('description', instance.description)
         instance.owner = validated_data.get('owner', instance.owner)
         instance.location = validated_data.get('location', instance.location)
