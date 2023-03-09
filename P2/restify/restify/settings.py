f4e69fb39a6ffbaa725067f52ef38f47e3976854
Vincent Zhu <vincenthero16@gmail.com>
2023-03-08 19:16:55 -0500

    comment model change

--- P2/restify/restify/settings.py
+++ P2/restify/restify/settings.py
@@ -89,9 +89,10 @@ REST_FRAMEWORK = {
 }
 
 REST_FRAMEWORK = {
-    
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+      'rest_framework_simplejwt.authentication.JWTAuthentication',
+      'rest_framework.authentication.SessionAuthentication',
+      'rest_framework.authentication.BasicAuthentication',
     )
 }
 
