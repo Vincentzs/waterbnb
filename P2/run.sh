d7525a05c5e9b19e72b65eae080a300d566642db
Vincent Zhu <vincenthero16@gmail.com>
2023-03-13 23:07:06 -0400

    update sh

--- P2/run.sh
+++ P2/run.sh
@@ -1,1 +1,3 @@
+python3 ./restify/manage.py makemigrations
+python3 ./restify/manage.py migrate
 python3 ./restify/manage.py runserver
--- P2/startup.sh
+++ P2/startup.sh
@@ -1,5 +1,6 @@
 sudo apt update 
 sudo apt install -y python3-pip python3-virtualenv sqlite3
+source venv/bin/activate
 cd restify
 virtualenv venv
 pip3 install -r requirements.txt 
