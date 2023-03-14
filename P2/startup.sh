6361e824debb82c5d09d306bb6dededcfffc5633
Vincent Zhu <vincenthero16@gmail.com>
2023-03-13 23:10:22 -0400

    update sh

--- P2/startup.sh
+++ P2/startup.sh
@@ -1,6 +1,8 @@
 sudo apt update 
-sudo apt install -y python3-pip python3-virtualenv sqlite3
-source venv/bin/activate
-cd restify
+sudo apt install python3-pip
+sudo apt install sqlite3
+sudo apt install python3-virtualenv
 virtualenv venv
 pip3 install -r requirements.txt 
+source venv/bin/activate
+cd restify
