873ca245d444132d932929d86722e6510861e85a
Yun Ru <uu.ru@mail.utoronto.ca>
2023-04-09 12:49:12 -0400

    Settings py update,

--- P3/backend/restify/restify/settings.py
+++ P3/backend/restify/restify/settings.py
@@ -94,8 +94,8 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
